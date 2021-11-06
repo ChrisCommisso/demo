fde6d84d3685ab99f5c6302a4cac3063c057e01e
Chris Commisso <qwertykeebored@gmail.com>
2021-11-06 09:28:50 -0400

    big push

---
+++ sample/Assets/scripts/Cards.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Cards : MonoBehaviour
+{
+    public void Start()
+    {
+        print("starting");
+    }
+    public void followMouse() {//built in unity method
+      
+            transform.position = Input.mousePosition;
+       
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ sample/Assets/scripts/cardProducer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cardProducer : MonoBehaviour
+{
+    public GameObject parent;
+    public GameObject card;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("start");
+    }
+    void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.L)) 
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+    // FixedUpdate is called once per physics frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Instantiate(card, Input.mousePosition, Quaternion.identity,parent.transform);
+        }
+    }
+}
